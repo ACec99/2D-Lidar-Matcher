d6e03cfa4d830fe46c15303e3ba77b436f975863
Cecilia Assolito <assolito.1857897@studenti.uniroma1.it>
2022-06-29 19:53:24 +0200

    added subscribe in the main with the function LaserCallBack (so far without body = not implemented) and other components of the program

--- src/2D_Lidar_Matcher/src/2D_Lidar_Matcher.cpp
+++ src/2D_Lidar_Matcher/src/2D_Lidar_Matcher.cpp
@@ -27,6 +27,8 @@
 
 ros::Publisher vel_pub ;
 
+ros::Subscriber vel_sub;
+
 const Eigen::Isometry2f getTransform(const std::string& from, const std::string& to) {
 	
   Eigen::Isometry2f MTB = Eigen::Isometry2f::Identity();
@@ -51,13 +53,19 @@ const Eigen::Isometry2f getTransform(const std::string& from, const std::string&
   return MTB;
 }
 
+void LaserCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_in) {}
+
 int main(int argc, char **argv) {
 	
 	ros::init(argc, argv, "2D_Lidar_Matcher");
 	
 	ros::NodeHandle n; //initialization of a node
 	
+	vel_sub = n.subscribe("/base_scan",1000,LaserCallBack); //I read the lasercan and update the Pose2D
+																					
 	vel_pub = n.advertise<geometry_msgs::Pose2D>("/pose2D", 1000); //I write on the /pose2D topic the correct coordinates 
+	
+	ros::spin(); //if you are subscribing to messages, services or actions, you must call spin to process the event
 	
 	return 0;
 }
